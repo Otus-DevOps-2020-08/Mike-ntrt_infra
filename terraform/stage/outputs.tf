ffd5ca68082d0c596afe3e0e34ce38c510580d41
Mikhail Klyuev <c32exe@gmail.com>
2020-10-07 18:14:39 +0300

    fix stage

--- terraform/stage/outputs.tf
+++ terraform/stage/outputs.tf
@@ -4,6 +4,3 @@ output "external_ip_address_app" {
 output "external_ip_address_db" {
   value = module.db.external_ip_address_db
 }
-#output "external_lb_ip_address" {
-#  value = element(element(yandex_lb_network_load_balancer.app-lb.listener.*, 0).external_address_spec.*.address, 0)
-#}
